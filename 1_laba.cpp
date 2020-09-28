020574312bbfbef6bfe192db0aa510949a9ae056
kseniabrsv <68520114+kseniabrsv@users.noreply.github.com>
2020-09-28 12:33:04 +0700

    Update 1_laba.cpp

--- 1_laba.cpp
+++ 1_laba.cpp
@@ -1,124 +1,117 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
 
+class Matrix;
 //класс для столбцов матрицы
-class column
+class Column
 {
-    int size;
-    int **mtrx;
-    unsigned col_index;
-    public:
-        column(): size(0), col_index(0), mtrx(nullptr)
-        {}
-        
-        column(unsigned s, unsigned index, int **m): size(s), col_index(index), mtrx(m)
-        {}
+    Matrix &m;
+    const unsigned col_index;
+public:       
+    Column(unsigned index, Matrix &m1): col_index(index), m(m1)
+    {}
 
-        int& operator[](unsigned indx) const
-        {
-            if (indx > size)
-                throw;
-            return mtrx[indx - 1][col_index];
-        }
+    int& operator[](unsigned indx) const;
 };
 
 //класс для строк матрицы
-class raw
+class Raw
 {
-    int size;
-    int **mtrx;
-    unsigned r_index;
-    public:
-        raw(): size(0), r_index(0), mtrx(nullptr)
-        {}
-        
-        raw(unsigned s, unsigned index, int **m): size(s), r_index(index), mtrx(m)
-        {}
+    Matrix &m;
+    const unsigned r_index;
+public:
+    Raw(unsigned index, Matrix &m1): r_index(index), m(m1)
+    {}
 
-        int& operator[](unsigned indx) const
-        {
-            if (indx > size)
-                throw;
-            return mtrx[r_index][indx - 1];
-        }
+    int& operator[](unsigned indx) const;
 };
 
 class Matrix
 {
     unsigned size;
     int **mtrx;
-    public:
-        Matrix(): size(0), mtrx(nullptr)
-        {}
+    friend class Column;
+    friend class Raw;
+public:
+    Matrix(): size(0), mtrx(nullptr)
+    {}
 
-        Matrix(unsigned s)
+    Matrix(unsigned s)
+    {
+        size = s;
+        mtrx = new int* [size];
+        for (int i = 0; i < size; i++)
+            mtrx[i] = new int [size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                mtrx[i][j] = (i == j);   
+    }
+
+    Matrix(unsigned s, int* array)
+    {
+        size = s;
+        mtrx = new int* [size];
+        for (int i = 0; i < size; i++)
+            mtrx[i] = new int [size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                if (i == j)
+                    mtrx[i][j] = array[i];
+                else
+                    mtrx[i][j] = 0;
+                
+    }
+
+    Matrix(unsigned s, std::istream &type)
+    {
+        size = s;
+        mtrx = new int* [size];
+        for (int i = 0; i < size; i++)
+            mtrx[i] = new int [size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                type >> mtrx[i][j];
+    }
+
+    ~Matrix()
+    {
+        for (int i = 0; i < size; i++)
+            delete [] mtrx[i];
+        delete [] mtrx;
+    }
+
+    Matrix& operator=(const Matrix& orig)
+    {
+        if (this != &orig)
         {
-            size = s;
+            this->~Matrix();
+            size = orig.size;
             mtrx = new int* [size];
             for (int i = 0; i < size; i++)
                 mtrx[i] = new int [size];
             for (int i = 0; i < size; i++)
                 for (int j = 0; j < size; j++)
-                    if (i == j)
-                        mtrx[i][j] = 1;
-                    else
-                        mtrx[i][j] = 0;     
+                    mtrx[i][j] = orig.mtrx[i][j];
         }
+        return *this;
+    }
 
-        Matrix(unsigned s, int* array)
-        {
-            size = s;
-            mtrx = new int* [size];
-            for (int i = 0; i < size; i++)
-                mtrx[i] = new int [size];
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
-                    if (i == j)
-                        mtrx[i][j] = array[i];
-                    else
-                        mtrx[i][j] = 0;
-                    
-        }
+    Matrix(const Matrix& orig): size(orig.size)
+    {
+        *this = orig;
+    }
 
-        ~Matrix()
-        {
-            for (int i = 0; i < size; i++)
-                delete [] mtrx[i];
-            delete [] mtrx;
-        }
-
-        Matrix& operator=(const Matrix& orig)
-        {
-            if (this != &orig)
-            {
-                this->~Matrix();
-                mtrx = new int* [size];
-                for (int i = 0; i < size; i++)
-                    mtrx[i] = new int [size];
-                for (int i = 0; i < size; i++)
-                    for (int j = 0; j < size; j++)
-                        mtrx[i][j] = orig.mtrx[i][j];
-            }
-            return *this;
-        }
-
-        Matrix(const Matrix& orig): size(orig.size)
-        {
-            *this = orig;
-        }
-
-        void print() const;
-        Matrix operator+(const Matrix& other) const;
-        Matrix operator-(const Matrix& other) const;
-        Matrix operator*(const Matrix& other) const;
-        bool operator==(const Matrix& other) const;
-        Matrix operator!() const;
-        Matrix operator()(unsigned n, unsigned m) const;
-        raw operator[](unsigned n);
-        column operator()(unsigned n);
-        void scan_matrix(unsigned s);
-        void scan_matrix_f(unsigned s, char* fname);
+    void print() const;
+    Matrix operator+(const Matrix& other) const;
+    Matrix operator-(const Matrix& other) const;
+    Matrix operator*(const Matrix& other) const;
+    bool operator==(const Matrix& other) const;
+    bool operator!=(const Matrix& other) const;
+    Matrix operator!() const;
+    Matrix operator()(unsigned n, unsigned m) const;
+    Raw operator[](unsigned n);
+    Column operator()(unsigned n);
+    friend std::istream &operator>>(std::istream &type, Matrix &m);
 };
 //функция для вывода матрицы на экран
 void Matrix::print() const
@@ -126,8 +119,8 @@ void Matrix::print() const
     for (int i = 0; i < size; i++)
     {
         for (int j = 0; j < size; j++)
-            cout << mtrx[i][j] << ' ';
-        cout << '\n';
+            std::cout << mtrx[i][j] << ' ';
+        std::cout << std::endl;
     }       
 }
 
@@ -135,13 +128,9 @@ Matrix Matrix::operator+(const Matrix& other) const
 {
     if (size!=other.size)
     {
-        throw;
+        throw "Размерности матриц не совпадают.";
     }
-    Matrix new_m;
-    new_m.size = size;
-    new_m.mtrx = new int* [size];
-    for (int i = 0; i < size; i++)
-        new_m.mtrx[i] = new int [size];
+    Matrix new_m(size);
     for (int i = 0; i < size; i++)
         for (int j = 0; j < size; j++)
             new_m.mtrx[i][j] = mtrx[i][j] + other.mtrx[i][j];
@@ -152,13 +141,9 @@ Matrix Matrix::operator-(const Matrix& other) const
 {
     if (size!=other.size)
     {
-        throw;
+        throw "Размерности матриц не совпадают.";
     }
-    Matrix new_m;
-    new_m.size = size;
-    new_m.mtrx = new int* [size];
-    for (int i = 0; i < size; i++)
-        new_m.mtrx[i] = new int [size];
+    Matrix new_m(size);
     for (int i = 0; i < size; i++)
         for (int j = 0; j < size; j++)
             new_m.mtrx[i][j] = mtrx[i][j] - other.mtrx[i][j];
@@ -169,13 +154,9 @@ Matrix Matrix::operator*(const Matrix& other) const
 {
     if (size!=other.size)
     {
-        throw;
+        throw "Размерности матриц не совпадают.";
     }
-    Matrix new_m;
-    new_m.size = size;
-    new_m.mtrx = new int* [size];
-    for (int i = 0; i < size; i++)
-        new_m.mtrx[i] = new int [size];
+    Matrix new_m(size);
     for (int i = 0; i < size; i++)
         for (int j = 0; j < size; j++)
         {
@@ -196,14 +177,29 @@ bool Matrix::operator==(const Matrix& other) const
                 return false;
     return true;
 }
+
+bool Matrix::operator!=(const Matrix& other) const
+{
+    if (size!=other.size)
+        return true;
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            if (mtrx[i][j] == other.mtrx[i][j])
+                return false;
+    return true;
+}
+
+std::istream & operator>>(std::istream &type, Matrix &m)
+{
+    for (int i = 0; i < m.size; i++)
+        for (int j = 0; j < m.size; j++)
+            type >> m.mtrx[i][j];
+    return type;
+}
 //оператор нахождения обратной матрицы
 Matrix Matrix::operator!() const
 {
-    Matrix new_m;
-    new_m.size = size;
-    new_m.mtrx = new int* [size];
-    for (int i = 0; i < size; i++)
-        new_m.mtrx[i] = new int [size];
+    Matrix new_m(size);
     for (int i = 0; i < size; i++)
         for (int j = 0; j < size; j++)
         {
@@ -215,12 +211,8 @@ Matrix Matrix::operator!() const
 Matrix Matrix::operator()(unsigned n, unsigned m) const
 {
     if (n > size || m > size)
-        throw;
-    Matrix new_m;
-    new_m.size = size - 1;
-    new_m.mtrx = new int* [new_m.size];
-    for (int i = 0; i < new_m.size; i++)
-        new_m.mtrx[i] = new int [new_m.size];
+        throw "Индекс выходит за границы массива.";
+    Matrix new_m(size - 1);
     for (int i = 0; i < n-1; i++)
         for (int j = 0; j < m-1; j++)
             new_m.mtrx[i][j] = mtrx[i][j];
@@ -229,64 +221,61 @@ Matrix Matrix::operator()(unsigned n, unsigned m) const
             new_m.mtrx[i-1][j-1] = mtrx[i][j];
     return new_m;
 }
-//считывание матрицы с консоли
-void Matrix::scan_matrix(unsigned s)
-{
-    this->~Matrix();
-    size = s;
-    mtrx = new int* [size];
-    for (int i = 0; i < size; i++)
-        mtrx[i] = new int [size];
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            cin >> mtrx[i][j];
-}
-//считывание матрицы из файла
-void Matrix::scan_matrix_f(unsigned s, char* fname)
-{
-    this->~Matrix();
-    size = s;
-    mtrx = new int* [size];
-    for (int i = 0; i < size; i++)
-        mtrx[i] = new int [size];
-    ifstream fin(fname);
-    for (int i = 0; i < size; i++)
-        for (int j = 0; j < size; j++)
-            fin >> mtrx[i][j];
-    fin.close();
-}
+
 //обращение к строке матрицы
-raw Matrix::operator[](unsigned n)
+Raw Matrix::operator[](unsigned n)
 {
     if (n > size)
-        throw;
-    return {size, n - 1, mtrx};
+        throw "Индекс выходит за границы массива.";
+    return {n - 1, *this};
 }
 //обращение к столбцу матрицы
-column Matrix::operator()(unsigned n)
+Column Matrix::operator()(unsigned n)
 {
     if (n > size)
-        throw;
-    return {size, n - 1, mtrx};
+        throw "Индекс выходит за границы массива.";
+    return {n - 1, *this};
+}
+
+int& Column::operator[](unsigned indx) const
+{
+    if (indx > m.size)
+        throw "Индекс выходит за границы массива.";
+    return m.mtrx[indx - 1][col_index];
+}
+
+int& Raw::operator[](unsigned indx) const
+{
+    if (indx > m.size)
+        throw "Индекс выходит за границы массива.";
+    return m.mtrx[r_index][indx - 1];
 }
 
 int main()
 {
-     unsigned n;
+    unsigned n;
     int k;
-    cin >> n;
-    cin >> k;
+    std::cin >> n;
+    std::cin >> k;
     int *k1 = new int [n];
     for (int i = 0; i < n; i++)
         k1[i] = k;
     Matrix km(n, k1);
-    Matrix am, bm, cm, dm;
-    am.scan_matrix(n);
-    bm.scan_matrix(n);
-    cm.scan_matrix(n);
-    dm.scan_matrix(n);
-    Matrix answer = (am + bm * (!cm) + km) * (!dm);
-    answer.print();
+    delete [] k1;
+    Matrix am(n), bm(n), cm(n), dm(n);
+    std::cin >> am;
+    std::cin >> bm;
+    std::cin >> cm;
+    std::cin >> dm;
+    try
+    {
+        Matrix answer = (am + bm * (!cm) + km) * (!dm);
+        answer.print();
+    }
+    catch(const char* msg)
+    {
+        std::cout << "Error: " << msg << std::endl;
+    }
     system("pause");
     return 0;
 }
