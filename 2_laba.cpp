866e57cdc6d4ec4416611b50783e94444e422bf7
kseniabrsv <68520114+kseniabrsv@users.noreply.github.com>
2020-10-21 17:24:25 +0700

    Update 2_laba.cpp

--- 2_laba.cpp
+++ 2_laba.cpp
@@ -7,10 +7,11 @@ class Expression
 {;
 public:
     friend Expression* read(string str);
-    friend Expression* read_without_brackets(string str);
+    friend Expression* simplify(Expression* ex);
     virtual ostream & print(ostream &type) const = 0;
     virtual Expression* derivative(string v) const = 0;
     virtual int eval(string str) const = 0;
+    virtual bool operator==(Expression* other) const = 0;
     virtual ~Expression()
     {}
 };
@@ -20,6 +21,7 @@ class Number: public Expression
 protected:
     int num;
 public:
+    friend Expression* simplify(Expression* ex);
     Number(int num): num(num) 
     {}
 
@@ -39,6 +41,15 @@ public:
         return num;
     }
 
+    bool operator==(Expression* other) const
+    {
+        Number* n = dynamic_cast<Number*>(other);
+        if (n == nullptr)
+            return false;
+        else
+            return (n->num == num);
+    }
+
     ~Number()
     {}
 };
@@ -48,6 +59,7 @@ class Variable: public Expression
 protected:
     string var;
 public:
+    friend Expression* simplify(Expression* ex);
     Variable(string var): var(var)
     {}
 
@@ -77,6 +89,15 @@ public:
         return answer;
     }
 
+    bool operator==(Expression* other) const
+    {
+        Variable* v = dynamic_cast<Variable*>(other);
+        if (v == nullptr)
+            return false;
+        else
+            return (v->var == var);
+    }
+
     ~Variable()
     {}
 };
@@ -90,9 +111,11 @@ protected:
     friend class Mul;
     friend class Div;
 public:
+    friend Expression* simplify(Expression* ex);
     Add(Expression* left, Expression* right): left(left), right(right)
     {}
 
+    bool operator==(Expression* other) const;
     ostream & print(ostream &type) const;
     int eval(string str) const;
     Expression* derivative(string v) const;
@@ -108,9 +131,11 @@ protected:
     friend class Mul;
     friend class Div;
 public:
+    friend Expression* simplify(Expression* ex);
     Sub(Expression *left, Expression *right): left(left), right(right)
     {}
 
+    bool operator==(Expression* other) const;
     ostream & print(ostream &type) const;
     int eval(string str) const;
     Expression* derivative(string v) const;
@@ -126,9 +151,11 @@ protected:
     friend class Sub;
     friend class Div;
 public:
+    friend Expression* simplify(Expression* ex);
     Mul(Expression *left, Expression *right): left(left), right(right)
     {}
 
+    bool operator==(Expression* other) const;
     ostream & print(ostream &type) const;
     int eval(string str) const;
     Expression* derivative(string v) const;
@@ -138,6 +165,7 @@ public:
 class Div: public Expression
 {
 protected:
+    friend Expression* simplify(Expression* ex);
     Expression *left;
     Expression *right;
     friend class Add;
@@ -147,6 +175,7 @@ public:
     Div(Expression *left, Expression *right): left(left), right(right)
     {}
 
+    bool operator==(Expression* other) const;
     ostream & print(ostream &type) const;
     int eval(string str) const;
     Expression* derivative(string v) const;
@@ -169,6 +198,15 @@ ostream & Add::print(ostream &type) const
     return type;
 }
 
+bool Add::operator==(Expression* other) const
+{
+    Add* a = dynamic_cast<Add*>(other);
+    if (a == nullptr)
+        return false;
+    else
+        return (a->left == left && a->right == right);
+}
+
 int Add::eval(string str) const
 {
     return left->eval(str) + right->eval(str);
@@ -187,6 +225,15 @@ ostream & Sub::print(ostream &type) const
     right->print(type);
     type << ")";
     return type;
+}
+
+bool Sub::operator==(Expression* other) const
+{
+    Sub* s = dynamic_cast<Sub*>(other);
+    if (s == nullptr)
+        return false;
+    else
+        return (s->left == left && s->right == right);
 }
 
 int Sub::eval(string str) const
@@ -215,6 +262,15 @@ ostream & Mul::print(ostream &type) const
     return type;
 }
 
+bool Mul::operator==(Expression* other) const
+{
+    Mul* m = dynamic_cast<Mul*>(other);
+    if (m == nullptr)
+        return false;
+    else
+        return (m->left == left && m->right == right);
+}
+
 int Mul::eval(string str) const
 {
     return left->eval(str) * right->eval(str);
@@ -241,6 +297,15 @@ ostream & Div::print(ostream &type) const
     return type;
 }
 
+bool Div::operator==(Expression* other) const
+{
+    Div* d = dynamic_cast<Div*>(other);
+    if (d == nullptr)
+        return false;
+    else
+        return (d->left == left && d->right == right);
+}
+
 int Div::eval(string str) const
 {
     return left->eval(str) / right->eval(str);
@@ -258,28 +323,6 @@ Div::~Div()
     delete right;
 }
 
-Expression* read_without_brackets(string str)
-{
-    int pos = str.length() - 1;
-    while (pos != 0)
-    {
-        if (str[pos] == '+')
-            return new Add(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
-        if (str[pos] == '-')
-            return new Sub(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
-        pos--;
-    }
-    pos = str.length() - 1;
-    while (pos != 0)
-    {
-        if (str[pos] == '*')
-            return new Mul(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
-        if (str[pos] == '/')
-            return new Div(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
-        pos--;
-    }
-}
-
 Expression* read(string str)
 {
     int brackets = 0;
@@ -292,7 +335,26 @@ Expression* read(string str)
             if (str.find('+') == string::npos && str.find('-') == string::npos && str.find('*') == string::npos && str.find('/') == string::npos)
                 return new Variable(str);
             else
-                return read_without_brackets(str);
+            {
+                int pos = str.length() - 1;
+                while (pos != 0)
+                {
+                    if (str[pos] == '+')
+                        return new Add(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
+                    if (str[pos] == '-')
+                        return new Sub(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
+                    pos--;
+                }
+                pos = str.length() - 1;
+                while (pos != 0)
+                {
+                    if (str[pos] == '*')
+                        return new Mul(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
+                    if (str[pos] == '/')
+                        return new Div(read(str.substr(0, pos-1)), read(str.substr(pos+1, str.length())));
+                    pos--;
+                }
+            }
     }
     str = str.substr(1, str.length() - 2);
     while (pos <= str.length())
@@ -318,12 +380,77 @@ Expression* read(string str)
     }
 }
 
+Expression* simplify(Expression* ex)
+{
+    Number* num = dynamic_cast<Number*>(ex);
+    Variable* var = dynamic_cast<Variable*>(ex);
+    if (num != nullptr || var != nullptr)
+        return ex;
+    Add* add_exp = dynamic_cast<Add*>(ex);
+    if (add_exp != nullptr)
+    {
+        add_exp->left = simplify(add_exp->left);
+        add_exp->right = simplify(add_exp->right);
+        Number* l = dynamic_cast<Number*>(add_exp->left);
+        Number* r = dynamic_cast<Number*>(add_exp->right);
+        if (l != nullptr && r != nullptr)
+            return new Number(ex->eval(""));
+    }
+    Sub* sub_exp = dynamic_cast<Sub*>(ex);
+    if (sub_exp != nullptr)
+    {
+        sub_exp->left = simplify(sub_exp->left);
+        sub_exp->right = simplify(sub_exp->right);
+        Number* l = dynamic_cast<Number*>(sub_exp->left);
+        Number* r = dynamic_cast<Number*>(sub_exp->right);
+        if (l != nullptr && r != nullptr)
+            return new Number(ex->eval(""));
+    }
+    Mul* mul_exp = dynamic_cast<Mul*>(ex);
+    if (mul_exp != nullptr)
+    {
+        mul_exp->left = simplify(mul_exp->left);
+        mul_exp->right = simplify(mul_exp->right);
+        Number* l = dynamic_cast<Number*>(mul_exp->left);
+        Number* r = dynamic_cast<Number*>(mul_exp->right);
+        if (l != nullptr)
+        {
+            if (l->num == 0)
+                return new Number(0);
+            if (l->num == 1)
+                return mul_exp->right;
+        }
+        if (r != nullptr)
+        {
+            if (r->num == 0)
+                return new Number(0);
+            if (r->num == 1)
+                return mul_exp->right;
+        }
+        if (l != nullptr && r != nullptr)
+            return new Number(ex->eval(""));
+    }
+    Div* div_exp = dynamic_cast<Div*>(ex);
+    if (div_exp != nullptr)
+    {
+        div_exp->left = simplify(div_exp->left);
+        div_exp->right = simplify(div_exp->right);
+        Number* l = dynamic_cast<Number*>(div_exp->left);
+        Number* r = dynamic_cast<Number*>(div_exp->right);
+        if (l != nullptr && r != nullptr)
+            return new Number(ex->eval(""));
+    }
+    return ex;    
+}
+
 int main()
 {
     string str;
     std::cin >> str;
     Expression* e = read(str);
-    Expression* de = e->derivative("x");
+    Expression* de = simplify(e);
+
+    //Expression* de = e->derivative("x");
     de->print(cout);
     return 0;
 }
