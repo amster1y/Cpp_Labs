088e2033e360ed14ff721d7d51261df1faaa541a
Kseniya Borisova <68520114+kseniabrsv@users.noreply.github.com>
2020-12-23 20:35:41 +0700

    Update 5_laba.cpp

--- 5_laba.cpp
+++ 5_laba.cpp
@@ -7,11 +7,14 @@
 #include <vector>
 #include <chrono>
 #include <set>
+#include <atomic>
 
 std::mutex locker;
 std::mutex fin_locker;
+std::atomic<bool> flag_of_working(false);
+std::atomic<int> links_count(0);
 
-void working_with_files(std::queue<std::string>& links, int& links_count, std::set<std::string>& names_of_files)
+void working_with_files(std::queue<std::string>& links, std::set<std::string>& names_of_files)
 {
     std::ifstream input;
     std::string str, str1, new_link;
@@ -19,27 +22,33 @@ void working_with_files(std::queue<std::string>& links, int& links_count, std::s
     std::regex mask2("[0-9]*.html");
     std::smatch matched_links, matched_links2;
 
-    while (!links.empty())
+    while (true)
     {
-        std::lock_guard<std::mutex> lock1(locker);
+        while (links.empty())
+        {
+            if (!flag_of_working)
+                return;
+        }
+        std::unique_lock<std::mutex> lock1(locker);
         if (links.empty())
         {
             break;
         }
+        flag_of_working = true;
         str = links.front();
         links.pop();
-        links_count++;
-        lock1.~lock_guard();
+        lock1.unlock();
         while (std::regex_search(str, matched_links, mask))
         {
             str1 = matched_links[0];
             str = matched_links.suffix().str();
             bool search = std::regex_search(str1, matched_links2, mask2);
             str1 = matched_links2.str();
+            std::unique_lock<std::mutex> lock2(fin_locker);
             if (names_of_files.find(str1) == names_of_files.end())
             {
-                std::lock_guard<std::mutex> lock2(fin_locker);
                 input.open("test_data/" + str1);
+                links_count++;
                 while(input)
                 {
                     std::getline(input, new_link);
@@ -49,6 +58,7 @@ void working_with_files(std::queue<std::string>& links, int& links_count, std::s
                 names_of_files.insert(str1);
             }
         }
+        flag_of_working = false;
     }
 } 
 
@@ -58,7 +68,7 @@ int main()
     std::ifstream input;
     std::ofstream output;
     std::string first_link, str;
-    int x, links_count = 0;
+    int x;
     std::queue<std::string> links;
     std::vector<std::thread> threads;
     std::set<std::string> names_of_files;
@@ -69,6 +79,7 @@ int main()
 
     first_link = first_link.substr(7);
     input.open(first_link);
+    links_count++;
     while(input)
     {
         std::getline(input, str);
@@ -78,7 +89,8 @@ int main()
     names_of_files.insert(first_link.substr(10));
     for (int i = 0; i < x; i++)
     {
-        threads.emplace_back(working_with_files, std::ref(links), std::ref(links_count), std::ref(names_of_files));
+        flag_of_working = true;
+        threads.emplace_back(working_with_files, std::ref(links), std::ref(names_of_files));
     }
     for (int i = 0; i < x; i++)
         threads[i].join();
@@ -87,4 +99,4 @@ int main()
     return 0;
 }
 
-//Самое эффективное время выполнения программы при 4 потоках ~13 сек
+//Самое эффективное время выполнения программы при 7 работающих потоках ~9,239 сек
